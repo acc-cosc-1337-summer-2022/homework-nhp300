39a50539f31e03607a3ac9fde91bf0a165294a7d
nihar.patel <nihar.patel@g.austincc.edu>
2022-06-17 21:47:25 -0500

    Homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,41 @@
-//write include statement for decisions header
+#include "decisions.h"
+
+int get_grade_points(string letter_grade)
+{
+    int grade_points = 0;
+
+    if(letter_grade == "A" || letter_grade == "a")
+    {
+        grade_points = 4;   
+    }
+    else if (letter_grade == "B" || letter_grade == "b")
+     {
+        grade_points = 3;
+    }
+    else if(letter_grade == "C" || letter_grade == "c" )
+    {
+        grade_points = 2;
+    }
+    else if(letter_grade == "D" || letter_grade == "d")
+    {
+        grade_points = 1;
+    }
+    else if(letter_grade == "F" || letter_grade == "f")
+    {
+        grade_points = 0;
+    }
+    return grade_points;
+}
 
 
-//Write code for function(s) code here
+double calculate_gpa(int credit_hours, double credit_points)
+{
+  double gpa = -1;
+
+   if(credit_hours != 0)
+   {
+        gpa = credit_points/ credit_hours;
+   }
+
+    return gpa;
+}
