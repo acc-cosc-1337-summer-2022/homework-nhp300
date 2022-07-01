228f68bfd78eb826ddff79a05aa8f5cde87caf30
nhp300 <106638060+nhp300@users.noreply.github.com>
2022-06-30 21:01:10 -0500

    Homework 5 - in class walkthrough

--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,6 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("TEST ACCOUUNT BALANCE"){
+	Account account;
+	REQUIRE(account.get_balance() == 0);
+}
+
+TEST_CASE("Test account balance set via constructor"){
+	Account account(200);
+	REQUIRE(account.get_balance() == 100);
 }
